c022a20231ab43059b3ee12d4dd83864375b3295
Viktor Nagy <viktor.nagy1995@gmail.com>
2024-07-25 13:30:42 +0200

    feat: score board

--- main.go
+++ main.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	"log"
 	"os"
+	"time"
 
 	"gioui.org/app"
+	"gioui.org/layout"
 	"gioui.org/op"
 	"gioui.org/op/clip"
 	"gioui.org/op/paint"
 	"gioui.org/text"
+	"gioui.org/unit"
 	"gioui.org/widget/material"
 )
 
 func main() {
+
+	snake := Snake{body: []point{{0, 0}, {0, 1}, {0, 2}}}
+
 	go func() {
 		window := new(app.Window)
-		err := run(window)
+		err := run(window, &snake)
 		if err != nil {
 			log.Fatal(err)
 		}
 		os.Exit(0)
+	}()
+
+	ticker := time.NewTicker(time.Second)
+	go func() {
+		for range ticker.C {
+			now := time.Now()
+			fmt.Println(now)
+		}
 	}()
 	app.Main()
 }
@@ -30,7 +45,7 @@ type point struct {
 	x, y int
 }
 
-type snake struct {
+type Snake struct {
 	body []point
 }
 
@@ -40,8 +55,9 @@ func drawRedRect(ops *op.Ops) {
 	paint.PaintOp{}.Add(ops)
 }
 
-func run(window *app.Window) error {
+func run(window *app.Window, snake *Snake) error {
 	theme := material.NewTheme()
+
 	var ops op.Ops
 	for {
 		switch e := window.Event().(type) {
@@ -50,16 +66,40 @@ func run(window *app.Window) error {
 		case app.FrameEvent:
 			gtx := app.NewContext(&ops, e)
 
-			title := material.H1(theme, "Snake")
-
 			maroon := color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-			title.Color = maroon
 
-			title.Alignment = text.Middle
+			layout.Flex{
+				Axis:    layout.Horizontal,
+				Spacing: layout.SpaceStart,
+			}.Layout(gtx,
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						title := material.H5(theme, "Score:")
 
-			title.Layout(gtx)
+						title.Color = maroon
 
-			drawRedRect(&ops)
+						title.Alignment = text.Middle
+
+						return title.Layout(gtx)
+					},
+				),
+				layout.Rigid(
+					layout.Spacer{Height: unit.Dp(25)}.Layout,
+				),
+				layout.Rigid(
+					func(gtx layout.Context) layout.Dimensions {
+						score := material.H5(theme, fmt.Sprintf("%d", len(snake.body)))
+
+						score.Color = maroon
+
+						score.Alignment = text.Middle
+
+						return score.Layout(gtx)
+					},
+				),
+			)
+
+			// drawRedRect(&ops)
 
 			e.Frame(gtx.Ops)
 		}
